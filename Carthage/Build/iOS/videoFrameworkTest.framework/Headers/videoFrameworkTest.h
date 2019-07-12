7282bb23439f832151d6c37c7101cc8ddf34bdc1
Muhammad Ali <AliGorssi@muhammads-mbp.home>
2019-07-12 11:19:19 +0500

    first commit

---
+++ Carthage/Build/iOS/videoFrameworkTest.framework/Headers/videoFrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  videoFrameworkTest.h
+//  videoFrameworkTest
+//
+//  Created by Muhammad Ali on 11/07/2019.
+//  Copyright © 2019 KoolSoftwares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for videoFrameworkTest.
+FOUNDATION_EXPORT double videoFrameworkTestVersionNumber;
+
+//! Project version string for videoFrameworkTest.
+FOUNDATION_EXPORT const unsigned char videoFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <videoFrameworkTest/PublicHeader.h>
+
+
---
+++ videoFrameworkTest/BackgroundVideo.swift
@@ -0,0 +1,70 @@
+//
+//  BackgroundVideo.swift
+//  videoFrameworkTest
+//
+//  Created by Muhammad Ali on 11/07/2019.
+//  Copyright © 2019 KoolSoftwares. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+public class BackgroundVideo: UIView {
+    
+    var player: AVPlayer?
+    var videoNames = ["video1","video2"]
+    var videoExtension = "mp4"
+    var selectedVideo: String?
+    var videoIndex = 0
+    
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    private func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        selectedVideo = videoNames[0]
+        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil, queue: nil) { notification in
+            self.videoToBePlayed()
+        }
+        initializeVideoPlayerWithVideo()
+    }
+    
+    private func initializeVideoPlayerWithVideo() {
+        
+        print(selectedVideo!)
+        let videoString:String? = Bundle.main.path(forResource: selectedVideo, ofType: videoExtension)
+        guard let unwrappedVideoPath = videoString else {return}
+        
+        let videoUrl = URL(fileURLWithPath: unwrappedVideoPath)
+        self.player = AVPlayer(url: videoUrl)
+        let layer: AVPlayerLayer = AVPlayerLayer(player: player)
+        layer.frame = UIScreen.main.bounds
+        layer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        layer.opacity = 0.3
+        
+        self.layer.addSublayer(layer)
+        player?.play()
+        player?.isMuted = true
+    }
+    
+    private func videoToBePlayed(){
+        if self.videoIndex >= 0 && self.videoIndex < self.videoNames.count-1 {
+            self.videoIndex += 1
+            self.selectedVideo = self.videoNames[self.videoIndex]
+        }else{
+            self.videoIndex = 0
+            self.selectedVideo = self.videoNames[self.videoIndex]
+        }
+        self.initializeVideoPlayerWithVideo()
+    }
+}
